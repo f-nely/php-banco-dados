f1e711e6e1e1512f8ee32ed4308472908cc5fe22
nely de faria <nelly.nf54@gmail.com>
2021-01-24 18:39:41 -0300

    Adicionando comentários

--- php-database/src/mysqli/tratamento-erro.php
+++ php-database/src/mysqli/tratamento-erro.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * connect_errno: return an error code from the last connection error, if any
+ * connect_error: return the error description from the last connection error, if any
+ *
+ */
+
 try {
     $conexao = new mysqli('127.0.0.1', 'root', '704802', 'tw_php');
 
+    // Check connection
     if ($conexao->connect_errno) {
         throw new Exception($conexao->connect_error, $conexao->connect_errno);
     }
