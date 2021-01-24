802763c6f9d58ffe73945a4e153cfe60b54a19d5
nely de faria <nelly.nf54@gmail.com>
2021-01-24 15:37:39 -0300

    Inserindo aluno no banco de dados tw_php

--- php-database/src/pdo/pdo.php
+++ php-database/src/pdo/pdo.php
@@ -3,11 +3,16 @@
 try {
     $conexao = new PDO('mysql:host=127.0.0.1;dbname=tw_php', 'root', '704802');
 
+    /*Realizando primeira consulta com pdo
     $alunos = $conexao->query('SELECT * FROM alunos');
 
     foreach ($alunos as $aluno) {
         var_dump($aluno);
-    }
+    }*/
+    
+    $resultado = $conexao->query("INSERT INTO alunos (id, nome) VALUES (4, 'Nikita Popov')");
+
+    var_dump($resultado);
 
 } catch (PDOException $PDOException) {
     echo $PDOException->getMessage();
