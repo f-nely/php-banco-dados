1a6f5c07f57ed5b397a2f0fcfc1f36071a0e8673
nely de faria <nelly.nf54@gmail.com>
2021-01-23 22:43:53 -0300

    Consulta que retorna mais de um item

--- php-database/src/mysqli/parametrizada.php
+++ php-database/src/mysqli/parametrizada.php
@@ -2,19 +2,23 @@
 
 $conexao = new mysqli('127.0.0.1', 'root', '704802', 'tw_php');
 
-$id = "2 OR 1=1";
+$id1 = "1";
+$id2 = "2";
 
 $stmt = $conexao->stmt_init();
 
-$stmt->prepare('SELECT * FROM alunos WHERE id=?');
-$stmt->bind_param('i', $id);
+$stmt->prepare('SELECT * FROM alunos WHERE id=? OR id=?');
+$stmt->bind_param('ii', $id1, $id2);
 
 $stmt->execute();
 
 $stmt->bind_result($idAluno, $nomeAluno);
-$stmt->fetch();
 
-var_dump($idAluno, $nomeAluno);
+while ($stmt->fetch()) {
+    var_dump($idAluno, $nomeAluno);
+}
+
+
 
 /*
  * https://www.php.net/manual/pt_BR/class.mysqli-stmt.php
