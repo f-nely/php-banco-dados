19b51be158d073b5aeac8c119c3e054528f5af18
nely de faria <nelly.nf54@gmail.com>
2021-01-23 23:10:09 -0300

    Tratamento de exceções com MySQL

--- php-database/src/mysqli/tratamento-erro.php
+++ php-database/src/mysqli/tratamento-erro.php
@@ -1,15 +1,21 @@
 <?php
 
-$conexao = new mysqli('127.0.0.1', 'root', '704802', 'tw_php');
+try {
+    $conexao = new mysqli('127.0.0.1', 'root', '704802', 'tw_php');
 
-if ($conexao->connect_errno) {
-    echo "código:" . $conexao->connect_errno;
-    echo "<br>mensagem: " . $conexao->connect_error;
-}
+    if ($conexao->connect_errno) {
+        throw new Exception($conexao->connect_error, $conexao->connect_errno);
+    }
 
-$resul = $conexao->query('aa');
+    $resul = $conexao->query('SELECT * FROM alunos');
 
-if (!$resul) {
-    echo "código: " . $conexao->errno;
-    echo "<br>mensagem: " . $conexao->error;
+    if (!$resul) {
+        throw new Exception($conexao->error, $conexao->errno);
+    }
+
+    var_dump($resul->fetch_all());
+
+} catch (\Throwable $exception) {
+    echo "mensagem: " . $exception->getMessage();
+    echo "<br>codigo: " . $exception->getCode();
 }
