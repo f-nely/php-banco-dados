cebc608ac20f887f90cb4225c695862cf07a3609
nely de faria <nelly.nf54@gmail.com>
2021-01-24 15:48:09 -0300

    Usando o método setFetchMode da classe PDOStatement

--- php-database/src/pdo/pdo.php
+++ php-database/src/pdo/pdo.php
@@ -3,15 +3,9 @@
 try {
     $conexao = new PDO('mysql:host=127.0.0.1;dbname=tw_php', 'root', '704802');
 
-    /*Realizando primeira consulta com pdo
-    $alunos = $conexao->query('SELECT * FROM alunos');
-
-    foreach ($alunos as $aluno) {
-        var_dump($aluno);
-    }*/
-
     $resultado = $conexao->query('SELECT * FROM alunos');
 
+    $resultado->setFetchMode(PDO::FETCH_ASSOC);
     foreach ($resultado as $aluno) {
         var_dump($aluno);
     }
